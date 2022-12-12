ed50e87e0c448cec3bb1def84711400a4bdecb1d
Alexey_wl <allex_doma@mail.ru>
2022-12-12 17:22:47 +0800

    добавил задачу 3

---
+++ 3.py
@@ -0,0 +1,10 @@
+#Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+import os
+
+def del_word(word):
+    path ='1'+ os.sep + 'file.txt'
+    with open(path, 'r+', encoding='utf-8') as read_file:
+        stroka = read_file.readline().split()
+        stroka = " ".join(filter(lambda x: word not in x, stroka))
+        read_file.write("\n"+stroka)
+del_word("абв")
